225358c61faae40ab82ff5106d19a9a5c507db99
albert.yang <18438049166@163.com>
2021-02-05 17:30:13 +0800

    # 提交线程安全容器练习

--- src/test/java/com/albert/concurrent/book/chapterthree/ThreadSafeCollection_15.java
+++ src/test/java/com/albert/concurrent/book/chapterthree/ThreadSafeCollection_15.java
@@ -9,29 +9,39 @@ import java.util.concurrent.ConcurrentMap;
 
 /**
  * 线程安全的集合练习
+ *
  * @author Albert
  * @date 2021/1/18 下午6:22
  */
 public class ThreadSafeCollection_15 {
 
     public static void main(String[] args) {
-        Map<String,String> oldMap = Maps.newHashMap();
+
+        //***********************包装map***************************
+        Map<String, String> oldMap = Maps.newHashMap();
         //使用Collections对map进行线程同步封装
-        Map<String,String> safeMap = Collections.synchronizedMap(oldMap);
+        Map<String, String> safeMap = Collections.synchronizedMap(oldMap);
+        safeMap.put(null, "a");
+        System.out.println(safeMap);
 
-        //线程安全的Map
-        ConcurrentMap<Object, Object> map = Maps.newConcurrentMap();
-
-
+        //***********************包装list***************************
         ArrayList<Object> oldList = Lists.newArrayList();
         //使用Collections对list进行线程同步封装
         List<Object> safeList = Collections.synchronizedList(oldList);
 
+        //***********************vector***************************
         Vector<Integer> vector = new Vector<>();
         vector.add(1);
         vector.add(2);
-
         System.out.println(JsonUtil.toString(vector));
+
+        //***********************注意***************************
+        //线程安全的Map
+        ConcurrentMap<String, String> concurrentMap = Maps.newConcurrentMap();
+        //concurrentMap的key或者value==null都会报空指针
+        concurrentMap.put("a", null);
+        System.out.println(JsonUtil.toString(concurrentMap));
+
     }
 
 
